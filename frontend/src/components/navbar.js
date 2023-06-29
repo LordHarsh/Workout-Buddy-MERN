90c422c69fa7c241aabc4deadac1b5c3c6148953
Harsh Banka <harshbanka321@gmail.com>
2023-06-29 22:32:16 +0530

    added logo

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,36 +2,37 @@ import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 const Navbar = () => {
-    const { logout } = useLogout()
-    const { user } = useAuthContext()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
-    const handleClick = () => {
-        logout()
-    }
+  const handleClick = () => {
+    logout();
+  };
 
-    return (
-        <header>
-            <div className="container">
-                <Link to='/'>
-                    <h1>Workout Buddy</h1>
-                </Link>
-                <nav>
-                    {user && (
-                        <div>
-                        <span>{user.email}</span>
-                            <button onClick={handleClick}>Logout</button>
-                        </div>
-                    )}
-                    {!user && (
-                    <div>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/signup'>Signup</Link>
-                    </div>
-                    )}
-                </nav>
+  return (
+    <header>
+      <div className="container">
+          <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+          <img src="./logo192.png" alt="buddy" height={100} />
+            <h1>Workout Buddy</h1>
+          </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
             </div>
-        </header>
-    );
-}
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
